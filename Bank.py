3f2bdb60507e11719cf5b2c79e550d6100f956f3
Khushil-6 <47305253+Khushil-6@users.noreply.github.com>
2019-03-04 20:00:25 +0100

    Update Bank.py

--- Bank.py
+++ Bank.py
@@ -1,128 +1,205 @@
-'''class Line:
+'''
+The sample Username and Password to enter the bank is :
 
-    def __init__(self,cord1,cord2):
-        self.cord1 = cord1
-        self.cord2 = cord2
+Username = smith
+Password = 1252
 
-    def distance(self,cord1,cord2):
+The rules of the bank is you cannot  withdraw the whole ammout there should be atleast something in the account 
+No limits on depositing the money
 
-        distance = ((cord2[0] - cord1[0]) ** 2 + (cord2[1] - cord1[1]) ** 2) ** 0.5
-        return distance
-
-    def slope(self,cord1,cord2):
-        slope = ((cord2[1]-cord1[1])/(cord2[0]-cord1[0]))
-        return slope
-
-coordinate1 = (3,2)
-coordinate2 = (8,10)
-
-li = Line(coordinate1,coordinate2)
-
-print(li.distance(coordinate1,coordinate2))
-print(li.slope(coordinate1,coordinate2))'''
+'''
 
 
-class Cylinder:
-    pi = 3.14
-
-    def __init__(self,height = 1,radius = 1):
-        self.height = height
-        self.radius = radius
-
-    def volume(self,):
-        volume = (Cylinder.pi*((self.radius)**2)*self.height)
-        print(volume)
-
-    def surface_area(self):
-        area = (2*Cylinder.pi*self.radius*self.height) + (2*Cylinder.pi*self.radius*self.radius)
-        print(area)
 
 
-class Account:
-    def __init__(self,name,funds):
+
+
+import time  # Used to create  small effect when the user is logging in
+
+
+class Account:  # Main Class with User Data
+
+    def __init__(self, name, funds):
         self.name = name
         self.funds = funds
 
-    def __str__(self):
-        print(f"Account Owner : {self.name}")
-
-    def __len__(self):
-        print()
-
-    def owner(self):
-        print(self.name)
-
+    # Prints when called Check Balance
     def balance(self):
         print(self.funds)
 
-    def deposit(self,number):
-        print("Deposit Accepted")
+    # Deposit's the User's amount and add's it to the current balance
+    def deposit(self, number):
+        print("The Deposit amount was Accepted.\n")
         self.funds = self.funds + number
         return self.funds
 
-    def withdraw(self,number):
+    # Withdraw's the money user asked and subtracts it from the current balance
+    def withdraw(self, number):
         if self.funds > number:
-            print("Withdrawal accepted")
+            print("The Withdrawal amount was accepted.\n")
             self.funds = self.funds - number
             return self.funds
+
+        # If the user enter's an invalid amount to withdraw
+
         else:
-            print("Not enough funds ")
+            print("Sorry You Don't Have Enough Funds...\n ")
+    #
+    # def again(self):
+    #     input("Do you want to do any thing else [Y/N]:").lower()
 
-    def again(self):
-        input("Do you want to do any thing else [Y/N]:").lower()
+# List of Account Holder's in PyBANK
 
 
+users = {"jack": {"password": "9911" , "funds": 700},
+         "smith": {"password": "1252", "funds": 2000},
+         "don": {"password": "1232", "funds": 1230},
+         "alex": {"password": "8877", "funds": 1009},
+         "max": {"password": "0001", "funds": 1340},
+         }
 
-act1 = Account("User",100)
+name = 0  # Helps to control the loop for asking the user name and password
+turn = 0  # Helps to control the Withdraw, Deposit, Check balance conditions
+ask = 0   # Helps to control the Flow of asking the user if they want to do anything again
 
-turn = 0
+# The main Loop Which Controls the Program
+
 while True:
-    try:
-        print(act1)
-    except TypeError:
-        pass
-    if turn == 0:
-        user = input("What do you want to do ""\n""1)Withdraw""\n""2)Deposit""\n" "3)Check Balance : ")
-        print('\n'*100)
 
-        if user == "1":
-            wit_amount = int(input("How Much Do you want to withdraw : "))
-            act1.withdraw(wit_amount)
-            again1 = input("Do you want to do anything else [Y/N]: ").lower()
-            if again1 == "y":
-                turn = 0
-                print("\n" * 100)
-                continue
-            else:
-                turn = 1
-                break
-        if user == "2":
-            dep_amount = int(input("How Much Do you want to Deposit : "))
-            act1.deposit(dep_amount)
-            again_2 = input("Do you want to do anything else [Y/N]: ").lower()
-            if again_2 == "y":
-                turn = 0
-                print("\n" * 100)
-                continue
-            else:
-                turn = 1
-                break
+    # Prints the name of the bank and welcomes the user...
 
-        if user == "3":
-            print("The account balance is $",end ="")
-            act1.balance()
-            again = input("Do you want to do anything else [Y/N]: ").lower()
-            if again == "y":
-                turn = 0
-                print("\n"*100)
-                continue
-            else:
-                turn = 1
-                break
+    print("\t\tWELCOME TO PyBANK\n\n")
+
+    # Authenticates the user by asking them their Username and Password
+
+    while name == 0:
+        print("Please enter the following...")
+        username = input("Username : ").lower()
+        password = input("Password : ")
+
+        # Checks the condition and does not allow the Username to be empty
+
+        if len(username) > 0:
+            pass
+        else:
+            print("Username cannot be blank\n")
+            continue
+
+        # Checks the condition and does not allow the Password to be empty
+
+        if len(password) > 0:
+            pass
+        else:
+            print("Password cannot be blank\n")
+            continue
+
+        # Checks the user in the above Dictionary and authenticates
+
+        if username in users:
+            if password == users[username]["password"]:
+
+                # Used this loop to show a effect when the user is logged in
+
+                for i in range(1, 4):
+                    print(".")
+                    time.sleep(0.5)
+                print("Login successful")
+                print("\n")
+                name = 1
+
+        # If the user is not in the Dictionary does not allow them into the system
+
+        else:
+            print("Username or Password was wrong\n")
+            name = 0
+            continue
+
+            # act1 is the user
+
+        try:
+            act1 = Account(username, users[username]["funds"])
+        except KeyError:
+            print("Wrong password try again")
+            continue
+
+        # turn helps to control the flow of transaction control
+
+        while turn == 0:
+
+            # Asks the User What task do they want to perform..
+
+            user = input("What do you want to do select from the number below...""\n""1]Withdraw.""\n""2]Deposit.""\n" 
+                         "3]Check Balance.""\n""--> ")
+            print('\n\n')
+
+            # Allow's User to Withdraw money
+
+            if user == "1":
+                wit_amount = int(input("How Much Amount Do you want to Withdraw $: ""\n"))
+                act1.withdraw(wit_amount)
+
+                # Ask's they User if they want to Do any task again.
+
+                while ask == 0:
+                    again1 = input("Do you want to do anything else [Y/N]: ").lower()
+                    if again1 == "yes":
+                        turn = 0
+                        print("\n\n\n")
+                        break
+                    elif again1 == "no":
+                        turn = 1
+                        break
+                    else:
+                        print("Please only write yes or no.\n")
+                        ask = 0
+                        continue
+
+            # Allow's User to Deposit money
+
+            if user == "2":
+                dep_amount = int(input("How Much Amount Do you want to Deposit : ""\n"))
+                act1.deposit(dep_amount)
+
+                # Ask's they User if they want to Do any task again.
+
+                while ask == 0:
+                    again2 = input("Do you want to do anything else [Y/N]: ").lower()
+                    if again2 == "yes":
+                        turn = 0
+                        print("\n\n\n")
+                        break
+                    elif again2 == "no":
+                        turn = 1
+                        break
+                    else:
+                        print("Please only write yes or no.\n")
+                        ask = 0
+                        continue
+
+            # Allow's User to Check Balance.
+
+            if user == "3":
+                print("The Current Account balance is $",end ="")
+                act1.balance()
+
+                # Ask's they User if they want to Do any task again.
+
+                while ask == 0:
+                    again = input("Do you want to do anything else [Y/N]: ").lower()
+                    if again == "yes":
+                        turn = 0
+                        print("\n\n\n")
+                        break
+                    elif again == "no":
+                        turn = 1
+                        break
+                    else:
+                        print("Please only write yes or no.\n")
+                        ask = 0
+                        continue
 
     else:
-        print("\n""Thank You Bye")
-
-
+        print("\n""Thank You For Using PyBank..")
+        break
 
 
